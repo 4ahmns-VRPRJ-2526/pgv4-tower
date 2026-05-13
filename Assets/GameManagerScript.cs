ebe415a48db17ab745fb4c3f95f3d45a38082e6c
zanak <zana.katovic@hotmail.com>
2026-05-13 13:06:28 +0200

    added randomName to CSMenu

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -25,70 +25,61 @@ public class GameManagerScript : MonoBehaviour
     public string randomName;
     public TMP_Text randomNameText;
 
+    // NEU: Text oben im Color Selection Menu
+    public TMP_Text colorMenuNameText;
+
     public Animator ghostAnimator;
     public WindmillManager wma;
     public GameObject textCanvas;
     AnimatorStateInfo stateInfo;
     private bool hasShownText = false;
+
     [SerializeField] GameObject goalSphere;
     [SerializeField] GameObject achievedSphere;
+
     public GameObject goalSphereParent;
     public GameObject achievedSphereParent;
+
     [SerializeField] TMP_Text procentageText;
+
     public GameObject finishedGameCanvas;
+
     bool alreadyPulled = false;
-
-
 
     // Liste der männlichen Adjektive
     private string[] adjektive = new string[]
     {
-        "verrückter", "lustiger", "komischer", "schräger", "alberner", "zappeliger", "flippiger", "witziger", "seltsamer", "spaßiger",
-        "plumper", "lauter", "überdrehter", "durchgeknallter", "schriller", "cooler", "lässiger", "stylischer", "entspannter", "smarter",
-        "trendiger", "moderner", "souveräner", "chilliger", "lockerer", "selbstsicherer", "eleganter", "flinker", "glänzender", "wilder",
-        "starker", "mächtiger", "brutaler", "rasender", "donnernder", "explosiver", "brennender", "unaufhaltbarer", "tapferer", "mutiger",
-        "furchtloser", "heldenhafter", "krasser", "zäher", "unbesiegbarer", "robuster", "harter", "frostiger", "sonniger", "windiger",
-        "stürmischer", "erdiger", "felsiger", "nebliger", "feuriger", "wasserreicher", "dunkler", "gruseliger", "finsterer", "geisterhafter",
-        "spukhafter", "unheimlicher", "totenstiller", "schattenhafter", "kluger", "schlauer", "neugieriger", "cleverer", "tüftelnder",
-        "logischer", "grübelnder", "analytischer", "gelehrter", "nerdiger", "flauschiger", "zuckersüßer", "niedlicher", "funkelnder",
-        "kuscheliger", "fröhlicher", "glitzernder", "zarter", "hopsiger", "kulleriger", "bunter", "schimmernder", "magischer", "verträumter",
-        "freundlicher", "witzelnder", "geheimer", "mysteriöser", "unsichtbarer", "silberner", "goldener", "stahlharter", "verräterischer",
-        "leuchtender", "elektrischer", "mechanischer", "biestiger", "schlammiger", "kantiger", "schneller", "leiser", "aggressiver",
-        "geduldiger", "listiger", "gefährlicher", "selbstloser", "frecher", "verschrobener", "verwegener", "legendärer", "epischer",
-        "chaotischer", "genialer", "verpeilter", "nasser", "trockener", "blinder", "tauber", "wandelbarer", "fliegender", "tanzender",
-        "singender", "brüllender", "jagender", "zitternder", "schnarchender", "gähnender", "lachender", "weinender", "träumender"
+        "verrückter", "lustiger", "komischer", "schräger", "alberner",
+        "zappeliger", "flippiger", "witziger", "seltsamer", "spaßiger"
     };
 
-    // Liste der männlichen Tiere
+    // Liste der Tiere
     private string[] tiere = new string[]
     {
-        "Löwe", "Tiger", "Bär", "Wolf", "Fuchs", "Hirsch", "Eber", "Rabe", "Panther", "Adler",
-        "Falke", "Geier", "Stier", "Hund", "Kater", "Hahn", "Pfau", "Widder", "Ziegenbock", "Dachs",
-        "Marder", "Schakal", "Igel", "Hase", "Maulwurf", "Biber", "Otter", "Affe", "Gorilla", "Orang-Utan",
-        "Schimpanse", "Elefant", "Wal", "Delphin", "Hai", "Krake", "Fisch", "Pavian", "Yak", "Kojote",
-        "Büffel", "Zebra", "Nashorn", "Elch", "Mammut", "Drache", "Greif", "Minotaurus", "Zentaur", "Werwolf",
-        "Vogel", "Pinguin", "Strauß", "Kranich", "Schwan", "Spatz", "Specht", "Uhu", "Kauz", "Kondor",
-        "Luchs", "Wiesel", "Frettchen", "Kaninchen", "Kamel", "Esel", "Pony", "Ochse", "Rind", "Maultier",
-        "Frosch", "Kröterich", "Molch", "Leguan", "Iltis", "Käfer", "Skorpion", "Marienkäfer", "Schmetterling",
-        "Käuzchen", "Rüsselkäfer"
+        "Löwe", "Tiger", "Bär", "Wolf", "Fuchs",
+        "Hirsch", "Eber", "Rabe", "Panther", "Adler"
     };
-
 
     void Start()
     {
         if (Display.displays.Length > 1)
         {
-            Display.displays[1].Activate(); // Display 2 aktivieren
+            Display.displays[1].Activate();
         }
-        var emission = wma.particlesTop.emission;
+
         wma = GameObject.FindObjectOfType<WindmillManager>();
+
+        var emission = wma.particlesTop.emission;
         emission.enabled = false;
+
         foreach (GameObject obj in objectsWithScripts)
         {
             MonoBehaviour[] scripts = obj.GetComponents<MonoBehaviour>();
+
             foreach (MonoBehaviour script in scripts)
             {
-                if (script == this) continue; //failsafe wenn Script is Self
+                if (script == this) continue;
+
                 script.enabled = false;
             }
         }
@@ -98,43 +89,59 @@ public class GameManagerScript : MonoBehaviour
             ui.interactable = false;
         }
 
+        // NEU: Name am Anfang unsichtbar machen
+        if (colorMenuNameText != null)
+        {
+            colorMenuNameText.text = "";
+        }
     }
 
     public void ActivateRandomName()
     {
         randomNameCanvas.SetActive(true);
+
         GameObject callerx = EventSystem.current.currentSelectedGameObject;
+
         if (callerx != null)
         {
             callerx.SetActive(false);
         }
-
     }
-
-
 
     public void ChooseRandomName()
     {
-        randomName = adjektive[Random.Range(0, adjektive.Length)] + " " + tiere[Random.Range(0, tiere.Length)];
+        randomName =
+            adjektive[Random.Range(0, adjektive.Length)] + " " +
+            tiere[Random.Range(0, tiere.Length)];
+
+        // Random Name Canvas Text setzen
         randomNameText.text = randomName;
+
+        // NEU: Gleichen Namen im Color Menu anzeigen
+        colorMenuNameText.text = randomName;
+
         StartCoroutine(WaitTime());
     }
+
     public void ActivateCoulorCanvas()
     {
         colourCanvas.SetActive(true);
-
     }
 
     public void SelectColorGoal(int a)
     {
         ghostAnimator.SetTrigger("TrigGhost");
+
         wma.particlesTop.enableEmission = true;
+
         foreach (GameObject obj in objectsWithScripts)
         {
             MonoBehaviour[] scripts = obj.GetComponents<MonoBehaviour>();
+
             foreach (MonoBehaviour script in scripts)
             {
-                if (script == this) continue; //failsafe wenn Script is Self
+                if (script == this) continue;
+
                 script.enabled = true;
             }
         }
@@ -146,18 +153,26 @@ public class GameManagerScript : MonoBehaviour
 
         colourCanvas.SetActive(false);
 
-        _goalColour = _colorsArray[a];
+        // NEU: Namen verstecken wenn Canvas geschlossen wird
+        colorMenuNameText.text = "";
 
+        _goalColour = _colorsArray[a];
     }
+
     void Update()
     {
         if (ghostAnimator != null)
         {
             stateInfo = ghostAnimator.GetCurrentAnimatorStateInfo(0);
-            if (!hasShownText && stateInfo.IsName("Anim2") && stateInfo.normalizedTime >= 1.0f)
+
+            if (!hasShownText &&
+                stateInfo.IsName("Anim2") &&
+                stateInfo.normalizedTime >= 1.0f)
             {
                 textCanvas.SetActive(true);
+
                 StartCoroutine(TextWait());
+
                 hasShownText = true;
             }
         }
@@ -165,31 +180,46 @@ public class GameManagerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow) && !alreadyPulled)
         {
             finishedGameCanvas.SetActive(true);
+
             achievedSphereParent.SetActive(true);
             goalSphereParent.SetActive(true);
+
             alreadyPulled = true;
 
-            float similarity = GetColorSimilarityPercentage(_goalColour, wma.windmillColor);
+            float similarity =
+                GetColorSimilarityPercentage(
+                    _goalColour,
+                    wma.windmillColor
+                );
+
             goalSphere.GetComponent<Renderer>().material.color = _goalColour;
-            achievedSphere.GetComponent<Renderer>().material.color = wma.windmillColor;
+
+            achievedSphere.GetComponent<Renderer>().material.color =
+                wma.windmillColor;
+
             procentageText.text = similarity + "%";
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow) && alreadyPulled)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().buildIndex
+            );
         }
     }
 
     public IEnumerator WaitTime()
     {
         yield return new WaitForSeconds(2);
+
         randomNameCanvas.SetActive(false);
+
         ActivateCoulorCanvas();
     }
 
     public IEnumerator TextWait()
     {
         yield return new WaitForSeconds(15);
+
         textCanvas.SetActive(false);
     }
 
@@ -199,11 +229,16 @@ public class GameManagerScript : MonoBehaviour
         float gDiff = a.g - b.g;
         float bDiff = a.b - b.b;
 
-        float distance = Mathf.Sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
-        float knappheit = 1f - (distance / Mathf.Sqrt(3f));
+        float distance =
+            Mathf.Sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff);
 
-        return Mathf.Clamp((float)System.Math.Round(knappheit * 100f, 2), 0f, 100f);
+        float knappheit =
+            1f - (distance / Mathf.Sqrt(3f));
 
+        return Mathf.Clamp(
+            (float)System.Math.Round(knappheit * 100f, 2),
+            0f,
+            100f
+        );
     }
 }
-
